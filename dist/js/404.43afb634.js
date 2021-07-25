2b0a26cd8385fab2b8375b37cda6831d1532ff88
Reza <rezamajidi.1992@gmail.com>
2021-07-26 00:08:06 +0430

    build assets

---
+++ dist/js/404.43afb634.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["404"],{"8cdb":function(t,e,n){"use strict";n.r(e);var a=function(){var t=this,e=t.$createElement;t._self._c;return t._m(0)},s=[function(){var t=this,e=t.$createElement,n=t._self._c||e;return n("div",{staticClass:"about"},[n("div",{staticClass:"container mx-auto"},[n("h1",{staticClass:"text-green-800 text-4xl text-center w-full"},[t._v("404")])])])}],c={name:"404"},l=c,r=n("2877"),i=Object(r["a"])(l,a,s,!1,null,null,null);e["default"]=i.exports}}]);
+//# sourceMappingURL=404.43afb634.js.map
